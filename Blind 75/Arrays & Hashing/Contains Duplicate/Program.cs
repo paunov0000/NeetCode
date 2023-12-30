e731e1e3dac91c032db35b8affd07523811ecbf9
paunov0000 <legendarygriefer96@gmail.com>
2023-12-30 20:34:08 +0200

    task 01 completed

---
+++ Blind 75/Arrays & Hashing/Contains Duplicate/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split(',').Select(i => int.Parse(i)).ToArray();
+        Console.WriteLine(ContainsDuplicate(input));
+    }
+
+    public static bool ContainsDuplicate(int[] nums)
+    {
+        var dictionary = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+
+            if (dictionary.ContainsKey(nums[i]))
+            {
+                return true;
+            }
+
+            dictionary.Add(nums[i], 1);
+        }
+
+        return false;
+    }
+}
+
+
+
